91c5af037d0bf15efe6cd94f454e28a61c75af4f
zahra alipour <z.alipour7034891@gmail.com>
2026-08-31 15:52:10 +0430

    update the init file

---
+++ exercises/__init__.py
@@ -0,0 +1,18 @@
+from .base import BaseExerciseChecker
+from .mini_squat import MiniSquatChecker
+from .shoulder_flexion import ShoulderFlexionChecker
+from .shoulder_abduction import ShoulderAbductionChecker
+from .biceps_curl import BicepsCurlChecker
+from .calf_raise import CalfRaiseChecker
+from .single_leg_stance import SingleLegStanceChecker
+
+
+__all__ = [
+    "BaseExerciseChecker",
+    "MiniSquatChecker",
+    "ShoulderFlexionChecker",
+    "ShoulderAbductionChecker",
+    "BicepsCurlChecker",
+    "CalfRaiseChecker",
+    "SingleLegStanceChecker",
+]
